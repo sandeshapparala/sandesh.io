284b897db8ae3c3a0af0d4c3de6422b377ca3778
sandeshapparala <sandeshapparala@gmail.com>
2024-11-11 15:36:17 +0530

    initial setup

---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Hero from "@/components/pagecomponents/Hero";
+
+const Page = () => {
+    return (
+        <div>
+            <Hero/>
+        </div>
+    )
+}
+export default Page
---
+++ src/components/pagecomponents/Hero.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className={"min-h-screen bg-white"}>
+
+        </div>
+    )
+}
+export default Hero
